e49178bd31c7263b568487b7022fe242e4bc4833
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-04-18 20:57:16 +0530

    HStack

--- DemoApp/TextInSwiftUI.swift
+++ DemoApp/TextInSwiftUI.swift
@@ -10,8 +10,8 @@ import SwiftUI
 struct TextInSwiftUI: View {
     var body: some View {
         VStack{
-            
-        }
+            Text("Welcome to SwiftUI").foregroundColor(.white)
+        }.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
     }
 }
 
