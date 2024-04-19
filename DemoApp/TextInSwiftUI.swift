26781f6e3347b905e4803514699ea0938055afd3
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-04-19 17:41:07 +0530

    created horizontal card view of icon and text

--- DemoApp/TextInSwiftUI.swift
+++ DemoApp/TextInSwiftUI.swift
@@ -9,9 +9,45 @@ import SwiftUI
 
 struct TextInSwiftUI: View {
     var body: some View {
+        // Column/ Layout
         VStack{
-            Text("Welcome to SwiftUI").foregroundColor(.white)
-        }.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
+            
+            // Text
+            Text("Welcome to SwiftUI").foregroundColor(.blue).font(.largeTitle)
+                .bold().fontDesign(.rounded)
+            Text("A modern approach towards UI").font(.system(size: 18)).foregroundColor(.black)
+            Spacer().frame(height: 30)
+            
+            // Parent HStack
+            HStack{
+                
+                // Child HStack - Home
+                Spacer()
+                HStack{
+                    Image("home").resizable().scaledToFit().frame(width: 30, height: 30)
+                    Text("Home")
+                }
+                Spacer()
+                
+                // Child HStack - Plane
+                HStack{
+                    Image("plane").resizable().scaledToFit().frame(width: 30, height: 30)
+                    Text("Travel")
+                }
+                Spacer()
+                
+                // Child Stack - Food
+                HStack{
+                    Image("soup").resizable().scaledToFit()
+                        .frame(width: 30, height: 30)
+                    Text("Food")
+                }
+                Spacer()
+                
+                
+            }.frame(maxWidth: .infinity)
+            
+        }.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity).padding()
     }
 }
 
