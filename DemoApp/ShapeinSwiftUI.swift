8269a6c8d0a6a72f9da98eaecaca3273eb8f4cba
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-04-19 19:34:54 +0530

    created Rectangle card

---
+++ DemoApp/ShapeinSwiftUI.swift
@@ -0,0 +1,18 @@
+//
+//  ShapeinSwiftUI.swift
+//  DemoApp
+//
+//  Created by Mrinmoy Borah on 19/04/24.
+//
+
+import SwiftUI
+
+struct ShapeinSwiftUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShapeinSwiftUI()
+}
