9650153c6efb232cdd460c49d4d7fe40b69c0257
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-04-24 23:15:56 +0530

    commit

--- DemoApp/State/Counter.swift
+++ DemoApp/State/Counter.swift
@@ -5,11 +5,18 @@
 //  Created by Mrinmoy Borah on 24/04/24.
 //
 
+// State:
+// 1. It stores the value that the view depends on
+// 2. State properties represent the values that can change
+// 3. When the values of a state property change UI automatically recomputes itself
+
 import SwiftUI
 
 struct Counter: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        @State var counter: Int = 0 //@ Property Wrapper
+        Text("Counter Value: \(counter)")
     }
 }
 
