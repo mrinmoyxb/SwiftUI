15bb69437b8bbd98448ed3a65af587cfafae891a
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-04-24 23:30:40 +0530

    binding

--- DemoApp/State/Counter.swift
+++ DemoApp/State/Counter.swift
@@ -10,13 +10,22 @@
 // 2. State properties represent the values that can change
 // 3. When the values of a state property change UI automatically recomputes itself
 
+// Binding:
+// 1. Put a $ in front of State property reference in our view code for binding
+
 import SwiftUI
 
 struct Counter: View {
     var body: some View {
         
         @State var counter: Int = 0 //@ Property Wrapper
-        Text("Counter Value: \(counter)")
+        @State var counterVal: String = "" // For TextField binding
+        
+        VStack(alignment: .center){
+            Text("Counter Value: \(counter)").font(.title).padding()
+            TextField("Enter your age: ", text: $counterVal).font(.title).padding()
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+        
     }
 }
 
