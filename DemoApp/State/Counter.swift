8859711d5e765d0e171a0fe46b6e23b463b3ebed
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-04-24 23:12:31 +0530

    state in Ui

---
+++ DemoApp/State/Counter.swift
@@ -0,0 +1,18 @@
+//
+//  Counter.swift
+//  DemoApp
+//
+//  Created by Mrinmoy Borah on 24/04/24.
+//
+
+import SwiftUI
+
+struct Counter: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Counter()
+}
