05a6481e74ef2573c559fee790afe8b432efe75c
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-04-25 19:34:09 +0530

    toggle button

---
+++ DemoApp/State/CustomToggle.swift
@@ -0,0 +1,18 @@
+//
+//  CustomToggle.swift
+//  DemoApp
+//
+//  Created by Mrinmoy Borah on 25/04/24.
+//
+
+import SwiftUI
+
+struct CustomToggle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomToggle()
+}
