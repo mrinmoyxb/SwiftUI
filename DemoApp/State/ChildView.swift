b26c0f9cd8a9ccb5e3c0890c228bbf7c92be708a
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-04-25 21:01:36 +0530

    binding

---
+++ DemoApp/State/ChildView.swift
@@ -0,0 +1,18 @@
+//
+//  ChildView.swift
+//  DemoApp
+//
+//  Created by Mrinmoy Borah on 25/04/24.
+//
+
+import SwiftUI
+
+struct ChildView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChildView()
+}
