2cc38d80fb55c061c9751f27530a189542542150
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-04-25 11:22:34 +0530

    commit counter UI

--- DemoApp/State/Counter.swift
+++ DemoApp/State/Counter.swift
@@ -12,6 +12,7 @@
 
 // Binding:
 // 1. Put a $ in front of State property reference in our view code for binding
+// 2. @Binding is a peoperty wrapper to indicate that it expects a binding to be passed to the view when an instance of the view is created
 
 import SwiftUI
 
@@ -19,11 +20,14 @@ struct Counter: View {
     var body: some View {
         
         @State var counter: Int = 0 //@ Property Wrapper
+        
         @State var counterVal: String = "" // For TextField binding
+        // $counterVal: we can use $ sign when referencing a state property to allow a view component to change the value in a state property.
         
         VStack(alignment: .center){
             Text("Counter Value: \(counter)").font(.title).padding()
             TextField("Enter your age: ", text: $counterVal).font(.title).padding()
+            Text("Age entered: \($counterVal)").font(.title).padding()
         }.frame(maxWidth: .infinity, maxHeight: .infinity)
         
     }
--- DemoApp/State/CounterButton.swift
+++ DemoApp/State/CounterButton.swift
@@ -8,8 +8,40 @@
 import SwiftUI
 
 struct CounterButton: View {
+    
+    // State Property
+    @State var counter: Int = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .center){
+            Spacer()
+            Text("Counter Value: \(counter)").font(.title)
+            Spacer()
+            // Increase in value of state property
+            Button(action: {counter += 1}){
+                Text("Increment")
+                    .frame(width: 200, height: 50)
+                    .font(.title).foregroundColor(.white)
+                    .background(.blue)
+                    .cornerRadius(10)
+            }
+            Button(action: {
+                if(counter>0){
+                    counter -= 1
+                }
+                else{
+                    counter = 0
+                }
+            }){
+                Text("Decrement")
+                    .frame(width: 200, height: 50)
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .background(.blue)
+                    .cornerRadius(10)
+            }
+            Spacer()
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
