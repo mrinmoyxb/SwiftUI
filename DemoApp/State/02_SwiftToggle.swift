4dcc2dbc0ca8462e581a08adc382483d2079f16b
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-04-26 10:32:44 +0530

    state and binding

---
+++ DemoApp/State/02_SwiftToggle.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftToggle.swift
+//  DemoApp
+//
+//  Created by Mrinmoy Borah on 26/04/24.
+//
+
+import SwiftUI
+
+struct SwiftToggle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftToggle()
+}
