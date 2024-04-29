50c657b370e690b1c52ea696f217160a4da60302
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-04-29 19:23:13 +0530

    created list in swift ui

---
+++ DemoApp/State/NavigationStack/Platform.swift
@@ -0,0 +1,18 @@
+//
+//  Platform.swift
+//  DemoApp
+//
+//  Created by Mrinmoy Borah on 29/04/24.
+//
+
+import SwiftUI
+
+struct Platform: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Platform()
+}
