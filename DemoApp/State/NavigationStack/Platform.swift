42a9e5ac88eae30b2e6ce88adef26614a62510d6
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-04-30 05:59:19 +0530

    navigation in swift ui

--- DemoApp/State/NavigationStack/Platform.swift
+++ DemoApp/State/NavigationStack/Platform.swift
@@ -13,29 +13,66 @@ struct PlatformScreen: View {
                                  .init(name: "Airplane", image: "plane", color: .blue),
                                  .init(name: "Home", image:"home" , color: .green)]
     
+    var details: [Details] = [.init(description: "Hot and Healthy", ratings: 4.9),
+                              .init(description: "Safe and Fast", ratings: 4.2),
+                              .init(description: "Best place", ratings: 4.3)]
+    
     var body: some View {
-        VStack{
-            //NavigationStack{
+        
+        // 1. Navigation Stack
+            NavigationStack{
                 List{
                     Section("Options"){
                         ForEach(platforms, id: \.name){platform in
-                            Label(
-                                title: { Text(platform.name).foregroundColor(platform.color) },
-                                icon: { Image(platform.image).resizable().scaledToFit().frame(width: 30, height: 20).foregroundColor(.black) }
-                            )
+                            
+                            // 2. Navigation Link is associated with data
+                            NavigationLink(value: platform){
+                                Label(
+                                    title: { Text(platform.name).foregroundColor(platform.color) },
+                                    icon: { Image(platform.image).resizable().scaledToFit().frame(width: 30, height: 20).foregroundColor(.black) }
+                                )
+                            }
                         }
                     }
-                }
+                    Section("Details"){
+                        ForEach(details, id: \.self){detail in
+                            NavigationLink(value: detail){
+                                LabeledContent(content: {Text(String(detail.ratings))}, label: {Text(detail.description)})
+                            }
+                        }
+                    }
+                }.navigationTitle("Travel & Tour")
+                
+                // 3. Destination Screen
+                    .navigationDestination(for: Platform.self){platform in
+                        ZStack{
+                            platform.color.ignoresSafeArea()
+                            Label(
+                                title: { Text(platform.name).bold().font(.largeTitle).foregroundColor(.white) },
+                                icon: { Image(platform.image).resizable().scaledToFit().frame(width: 50, height: 50).foregroundColor(.white) }
+                            )
+                        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+                            
+                    }
+                    .navigationDestination(for: Details.self){ detail in
+                        HStack{
+                            Text(String(detail.ratings))
+                            Text(detail.description)
+                        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+                    }
             }
-        //}
-        
-    }
+        }
 }
 
-struct Platform{
+struct Platform: Hashable{
     var name: String
     var image: String
     var color: Color
+}
+
+struct Details: Hashable{
+    var description: String
+    var ratings: Double
 }
 
 #Preview {
