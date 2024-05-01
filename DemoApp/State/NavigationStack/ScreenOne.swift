5a1b145a740cf63e44fba22ae9286a26d6f389f2
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-05-01 14:47:30 +0530

    navigation stack

---
+++ DemoApp/State/NavigationStack/ScreenOne.swift
@@ -0,0 +1,18 @@
+//
+//  ScreenOne.swift
+//  DemoApp
+//
+//  Created by Mrinmoy Borah on 30/04/24.
+//
+
+import SwiftUI
+
+struct ScreenOne: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScreenOne()
+}
---
+++ DemoApp/State/NavigationStack/ScreenThree.swift
@@ -0,0 +1,18 @@
+//
+//  ScreenThree.swift
+//  DemoApp
+//
+//  Created by Mrinmoy Borah on 01/05/24.
+//
+
+import SwiftUI
+
+struct ScreenThree: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScreenThree()
+}
