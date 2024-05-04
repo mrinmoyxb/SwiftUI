9573cea42fe2719d477f7e1ebb97e15499a8ee4c
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-05-04 17:54:10 +0530

    struct in swift

--- DemoApp/State/Struct/MyPlayground-Struct.playground/Contents.swift
+++ DemoApp/State/Struct/MyPlayground-Struct.playground/Contents.swift
@@ -1,3 +1,24 @@
-import UIKit
+//! Key Concept
+//* Init in struct
+import Foundation
 
-var greeting = "Hello, playground"
+struct Quiz{
+    let title: String
+    let isCreated: Date
+    let isPremium: Bool?
+
+    init(title: String, isCreated: Date = .now, isPremium: Bool?){
+        self.title = title
+        self.isCreated = isCreated
+        self.isPremium = isPremium
+    }
+
+    func display(){
+        print("Title: \(title)")
+        print("Is Created: \(isCreated)")
+        print("Is Premium: \(isPremium!)")
+    }
+}
+
+var q1 = Quiz(title: "X", isPremium: true)
+q1.display()
