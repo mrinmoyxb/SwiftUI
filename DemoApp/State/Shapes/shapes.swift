1be72f02946dcfa27763ff6f524e261a43e53332
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-06-15 08:16:32 +0530

    Revision State in SwiftUi

---
+++ DemoApp/State/Shapes/shapes.swift
@@ -0,0 +1,18 @@
+//
+//  shapes.swift
+//  DemoApp
+//
+//  Created by Mrinmoy Borah on 04/05/24.
+//
+
+import SwiftUI
+
+struct shapes: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    shapes()
+}
