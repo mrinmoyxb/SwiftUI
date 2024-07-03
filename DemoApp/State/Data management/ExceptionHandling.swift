b3395276e427eb2cbe18210e0086ff9e5bafbcee
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-07-03 23:47:05 +0530

    Async await

--- DemoApp/State/Data management/ExceptionHandling.swift
+++ DemoApp/State/Data management/ExceptionHandling.swift
@@ -6,10 +6,93 @@
 //
 
 import SwiftUI
+import Foundation
+
+class ExceptionManager{
+    
+    private var isActive: Bool = false
+    
+    func exceptionMessage() throws -> String {
+        if(isActive){
+            return "URL is Active"
+        }
+        else{
+            throw URLError(.badServerResponse)
+        }
+    }
+}
+
+class ExceptionViewModel: ObservableObject{
+    
+    @Published var message: String = ""
+    @Published var errorMessage: String = ""
+    
+    var manager: ExceptionManager = ExceptionManager()
+    
+    func manageData(){
+        do{
+            self.message = try manager.exceptionMessage()
+        } catch {
+            self.errorMessage = error.localizedDescription
+        }
+    }
+}
 
 struct ExceptionHandling: View {
+    
+    @StateObject var viewModel = ExceptionViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(spacing: 10){
+            
+            ZStack {
+                Rectangle()
+                    .frame(width: 240, height: 90)
+                    .foregroundColor(.blue)
+                
+                VStack {
+                    Text("Message")
+                        .foregroundColor(.white)
+                        .font(.title3)
+                        .fontWeight(.bold)
+                    
+                    Text(viewModel.message)
+                        .foregroundColor(.white)
+                        .font(.title2)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                }
+            }
+            
+            ZStack {
+                Rectangle()
+                    .frame(width: 240, height: 90)
+                    .foregroundColor(.green)
+                
+                VStack {
+                    Text("Error")
+                        .foregroundColor(.white)
+                        .font(.title3)
+                        .fontWeight(.bold)
+                    
+                    Text(viewModel.errorMessage)
+                        .foregroundColor(.white)
+                        .font(.title3)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                }
+            }
+            
+            Button{
+                viewModel.manageData()
+            }label:{
+                Text("Click here")
+                    .frame(width: 100, height: 50)
+                    .background(.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            
+        }
     }
 }
 
--- DemoApp/State/Data management/asyncawait.swift
+++ DemoApp/State/Data management/asyncawait.swift
@@ -6,10 +6,86 @@
 //
 
 import SwiftUI
+import Foundation
+
+struct GithubUser: Codable{
+    let login: String
+    let name: String
+    let avatar_url: String
+    let followers: Int
+    let following: Int
+    let bio: String
+}
+
+enum APIError: Error{
+    case InvalidUrl
+    case BadResponse
+}
+
+class APIManager{
+    
+    func getUserData() async throws -> GithubUser {
+        let api: String = "https://api.github.com/users/mrinmoyxb"
+        guard let url = URL(string: api) else{
+            throw APIError.InvalidUrl
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw APIError.BadResponse
+        }
+        
+        let gitUser = try JSONDecoder().decode(GithubUser.self, from: data)
+        return gitUser
+    }
+}
+
+class GithubUserViewModel: ObservableObject{
+    
+    @Published var user: GithubUser = GithubUser(login: "", name: "", avatar_url: "", followers: 0, following: 0, bio: "")
+    @Published var userError: String =  ""
+    private var apiManager = APIManager()
+    
+    init(){
+        Task{
+            do{
+                let fetchedUserData = try await apiManager.getUserData()
+                DispatchQueue.main.async{
+                    self.user = fetchedUserData
+                }
+            }
+            catch{
+                let errorMessage = error.localizedDescription
+                DispatchQueue.main.async{
+                    self.userError = errorMessage
+                }
+            }
+        }
+    }
+}
 
 struct asyncawait: View {
+    
+    @StateObject var viewModel: GithubUserViewModel = GithubUserViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            // heading
+            Text("Github Portfolio")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundColor(.blue)
+                .padding(.horizontal)
+            
+            // profile pic
+            
+            
+            // bio
+            Text("Name: \(viewModel.user.name)")
+                .lineLimit(1)
+                .multilineTextAlignment(.center)
+        }
     }
 }
 
