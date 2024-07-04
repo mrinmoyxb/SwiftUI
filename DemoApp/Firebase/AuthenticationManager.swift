24e62b0d4db583c2354161f8e0e99f656499d9e6
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-07-04 11:08:10 +0530

    firebase auth

---
+++ DemoApp/Firebase/AuthenticationManager.swift
@@ -0,0 +1,8 @@
+//
+//  AuthenticationManager.swift
+//  DemoApp
+//
+//  Created by Mrinmoy Borah on 04/07/24.
+//
+
+import Foundation
---
+++ DemoApp/Firebase/view/RootView.swift
@@ -0,0 +1,18 @@
+//
+//  RootView.swift
+//  DemoApp
+//
+//  Created by Mrinmoy Borah on 04/07/24.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RootView()
+}
